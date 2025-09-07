3c7cac81247cae54f27e70c3595dfb2386442b47
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-09-07 18:06:23 +0100

    refactor: reorganize error variable declarations for clarity

--- internal/core/service/resource/errors.go
+++ internal/core/service/resource/errors.go
@@ -3,18 +3,25 @@ package resource
 import "errors"
 
 var (
-	ErrDuplicateID         = errors.New("record with this ID already exists")
-	ErrEmptyRecordID       = errors.New("record ID cannot be empty")
-	ErrMismatchedID        = errors.New("ID in record and parameter provided ID do not match")
-	ErrNoDataProvided      = errors.New("no data provided")
-	ErrEmptyRecordKey      = errors.New("record key cannot be empty")
+	// id/key errors
+	ErrDuplicateID    = errors.New("record with this ID already exists")
+	ErrEmptyRecordID  = errors.New("record ID cannot be empty")
+	ErrMismatchedID   = errors.New("ID in record and parameter provided ID do not match")
+	ErrEmptyRecordKey = errors.New("record key cannot be empty")
+
+	// resource errors
 	ErrEmptyResourceName   = errors.New("resource name cannot be empty")
 	ErrWrongResourceType   = errors.New("operation not valid for this resource type")
+	ErrInvalidResourceName = errors.New("resource name is invalid")
 	ErrUnknownResourceType = errors.New("unknown resource type")
 	ErrResourceNotFound    = errors.New("top-level resource not found")
-	ErrRecordNotFound      = errors.New("record not found")
-	ErrInvalidRecord       = errors.New("record is invalid")
-	ErrInvalidResourceName = errors.New("resource name is invalid")
-	ErrInternal            = errors.New("internal server error")
-	ErrGettingAllRecords   = errors.New("cannot get all records")
+
+	// record errors
+	ErrGettingAllRecords = errors.New("cannot get all records")
+	ErrRecordNotFound    = errors.New("record not found")
+	ErrInvalidRecord     = errors.New("record is invalid")
+	ErrNoDataProvided    = errors.New("no data provided")
+
+	// others
+	ErrInternal = errors.New("internal server error")
 )

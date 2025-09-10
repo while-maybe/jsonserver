6a8bd458f47e3c1c21a545df0b83ace20e3f0eb7
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-09-10 21:26:58 +0100

    feat: refactor FilePersister to use dataDir for file management and improve cleanup logic

--- internal/adapters/driven/jsonrepo/persister.go
+++ internal/adapters/driven/jsonrepo/persister.go
@@ -3,7 +3,10 @@ package jsonrepo
 import (
 	"encoding/json/jsontext"
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
+	"strings"
 
 	jsonv2 "encoding/json/v2"
 )
@@ -16,25 +19,59 @@ type Persister interface {
 }
 
 type FilePersister struct {
-	filename string
+	dataDir string
 }
 
-func NewFilePersister(filename string) *FilePersister {
-	return &FilePersister{filename: filename}
+func NewFilePersister(dataDir string) *FilePersister {
+	return &FilePersister{dataDir: dataDir}
 }
 
 func (fp *FilePersister) Persist(data map[string]any) error {
-	f, err := os.OpenFile(fp.filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, defaultFilePermissions)
+	keysInCache := make(map[string]bool)
 
+	// figure the keys that should actually exist as individual json data files
+	for resourceName, resourceData := range data {
+		keysInCache[resourceName] = true
+
+		// generate filenames based on top-resource names
+		filename := fmt.Sprintf("%s.json", resourceName)
+		filePath := filepath.Join(fp.dataDir, filename)
+
+		file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, defaultFilePermissions)
+
+		if err != nil {
+			return fmt.Errorf("error opening file %s for persistence: %w", filePath, err)
+		}
+		defer file.Close()
+
+		opts := jsonv2.JoinOptions(jsontext.Multiline(true), jsontext.WithIndent("  "))
+		if err := jsonv2.MarshalWrite(file, resourceData, opts); err != nil {
+			return fmt.Errorf("error writing JSON to file %s: %w", filePath, err)
+		}
+	}
+
+	// cleanup phase
+	entries, err := os.ReadDir(fp.dataDir)
 	if err != nil {
-		return fmt.Errorf("opening file %s for persistence: %w", fp.filename, err)
+		return fmt.Errorf("failed to read data directory for cleanup: %w", err)
 	}
-	defer f.Close()
 
-	opts := jsonv2.JoinOptions(jsontext.Multiline(true), jsontext.WithIndent("  "))
+	for _, entry := range entries {
+		if entry.IsDir() || filepath.Ext(entry.Name()) != ".json" {
+			continue
+		}
 
-	if err := jsonv2.MarshalWrite(f, data, opts); err != nil {
-		return fmt.Errorf("writing JSON to file %s: %w", fp.filename, err)
+		resourceName := strings.TrimSuffix(entry.Name(), ".json")
+
+		// now if a file exists without a matching resourceName in-memory we delete
+		if _, exists := keysInCache[resourceName]; !exists {
+			filePath := filepath.Join(fp.dataDir, entry.Name())
+
+			if err := os.Remove(filePath); err != nil {
+				log.Printf("WARN: failed to remove orphaned resource file '%s': %v", filePath, err)
+			}
+		}
 	}
+
 	return nil
 }

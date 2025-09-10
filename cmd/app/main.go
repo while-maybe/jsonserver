83b8a09d17340022ffc11f7d2618d49492494d51
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-09-10 21:27:09 +0100

    feat: refactor main to load configuration and improve server initialization

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,35 +1,84 @@
 package main
 
 import (
+	"context"
 	"jsonserver/internal/adapters/driven/jsonrepo"
 	"jsonserver/internal/adapters/driving/httpadapter"
+	"jsonserver/internal/config"
 	"jsonserver/internal/core/service/resource"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 	log.Printf("Starting json server...")
 
-	dbFilename := "db.json"
-	listenAddr := ":8080"
-
-	repo, err := jsonrepo.NewJsonRepository(dbFilename)
+	// load the config
+	cfg, err := config.Load()
 	if err != nil {
-		log.Fatalf("Failed to initialize repository: %v", err)
+		log.Fatalf("Failed to load configuration: %v", err)
 	}
 
-	log.Printf("Repository for '%s' initialized successfully.", dbFilename)
+	log.Printf("Configuration loaded: Server Address=%s, Data Directory=%s", cfg.ServerAddr, cfg.DataDir)
 
+	// create the repo
+	repo, err := jsonrepo.NewJsonRepository(cfg.DataDir)
+	if err != nil {
+		log.Fatalf("Failed to create repository: %v", err)
+	}
+
+	log.Printf("Repository initialized successfully.")
+
+	// service and handler
 	resourceSvc := resource.NewService(repo)
+	apiHandler := httpadapter.NewHandler(resourceSvc)
 
-	httpHandler := httpadapter.NewHandler(resourceSvc)
-
-	router := httpHandler.SetupRoutes()
-
-	log.Printf("Server is ready to handle requests at %s", listenAddr)
-
-	if err := http.ListenAndServe(listenAddr, router); err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+	// config the server
+	server := &http.Server{
+		Addr:         cfg.ServerAddr,
+		Handler:      apiHandler.SetupRoutes(),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
+
+	// create the context
+	appCtx, cancel := context.WithCancel(context.Background())
+	// failsafe cleanup
+	defer cancel()
+
+	// start the file watcher
+	repo.Watch(appCtx)
+
+	// start the server
+	go func() {
+		log.Printf("Server starting on %s", cfg.ServerAddr)
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+	}()
+
+	log.Printf("Server is ready to handle requests at %s", cfg.ServerAddr)
+
+	// clean shutdown sequence
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM) // listen to OS interrupt signal
+
+	<-quit
+	log.Println("Shutdown signal received...")
+
+	cancel()
+	log.Println("Signaling background processes to stop...")
+
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdownCancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("FATAL: Server forced to shutdown: %v", err)
+	}
+
+	log.Println("Server exiting gracefully...")
 }
